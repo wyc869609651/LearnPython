948119ca5f523b95850e34a22deeaa23379bbf4c
wuyanchen <869606351@qq.com>
2018-01-15 23:07:52 +0800

    Home FunctionalProgram->ReturnFunc

--- c04_FunctionalProgram/le02_ReturnFunc.py
+++ c04_FunctionalProgram/le02_ReturnFunc.py
@@ -21,3 +21,86 @@ def lazy_sum(*args):
 # 当我们调用lazy_sum()时，返回的并不是求和结果，而是求和函数：
 f = lazy_sum(1, 3, 5, 7, 9)
 print(f)
+
+# 调用函数f时，才真正计算求和的结果
+print(f())
+
+# 在这个例子中，我们在函数lazy_sum中有定义的函数sum，并且，内部函数sum可以引用外部函数lazy_sum的参数和局部变量，
+# 当lazy_sum返回函数sum时，相关参数和变量都保存在返回的函数中，这种称为“闭包（Closure）”的程序结构拥有极大的威力。
+
+# 请再注意一点，当我们调用lazy_sum()时，每次调用都会返回一个新的函数，即使传入相同的参数：
+f1 = lazy_sum(1, 3, 5, 7, 9)
+f2 = lazy_sum(1, 3, 5, 7, 9)
+print(f1 == f2)
+# f1()和f2()的调用结果互不影响
+
+
+# 闭包
+# 注意到返回的函数在其定义内部引用了局部变量args，所以，当一个函数返回了一个函数后，其内部的局部变量还被新函数
+# 引用，所以，闭包用起来简单，实现起来可不容易。
+# 另一个需要注意的问题是，返回的函数并没有立即执行，而是等到调用f()才执行。我们来看个例子：
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+            return i * i
+        fs.append(f)
+    return fs
+
+f1, f2, f3 = count()
+# 在上面的例子中，每次循环，都创建了一个新的函数，然后，把创建的3个函数都返回了。
+# 你可能认为调用f1(),f2()和f3()结果应该是1，4，9，但实际结果是：
+print(f1())
+print(f2())
+print(f3())
+# 全部都是9！原因就在于返回的函数引用了变量i,但它并非立即执行。等到3个函数都返回时，它们所引用的变量i已经变成了3，
+# 因此最总终结果为9。
+# 注意：返回闭包时牢记一点：返回函数不要引用任何循环变量，或者后续会发生变化的变量。
+
+# 如果一定要引用循环变量怎么办？方法是再创建一个函数，用该函数的参数绑定循环变量的当前值，无论该循环变量后续如何更改，
+# 已绑定到函数参数的值不变：
+def count2():
+    def f(j):
+        def g():
+            return j * j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i))  # f(i)立刻执行，因此i的当前值被传入f()
+    return fs
+
+for f in count2():
+    print(f())
+
+# 小结
+# 一个函数可以返回一个计算结果，也可以返回一个函数。
+# 返回一个函数时，牢记该函数并未执行，返回函数中不要引用任何可能会变化的变量。
+
+# 练习1
+# 利用闭包返回一个计数器函数，每次调用它返回递增整数：
+def createCounter():
+    def natureNum():
+        i = 1
+        while True:
+            yield i
+            i = 1
+    o = natureNum()
+
+    def counter():
+        return next(o)
+    return counter
+
+def createCounter2():
+    n = 0
+    def counter():
+        nonlocal n
+        n = n + 1
+        return n
+    return counter
+
+counter = createCounter2()
+print(counter())
+print(counter())
+print(counter())
+print(counter())
+
